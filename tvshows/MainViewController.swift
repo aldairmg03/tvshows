c4ce1efabfec861c627643065481d2abea4ff8e8
aldairmg <amartinezg7455@gmail.com>
2022-12-02 09:28:44 -0600

    Main storyboard was removed

--- tvshows/MainViewController.swift
+++ tvshows/MainViewController.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController.swift
+//  MainViewController.swift
 //  tvshows
 //
 //  Created by Aldair Martínez on 02/12/22.
@@ -7,7 +7,7 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class MainViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
